a64b2a65e2e9993d09f3ac8583118c75ae722d65
Rizwan <RizwanA@expertdynamicsgroup.com>
2025-08-19 13:36:48 +0500

    connect database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 import express  from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB).then(()=>{
+  console.log('mongodb connected');
+}).catch((err)=>{
+  console.error('Error connecting to MongoDB:', err);
+})
+
 const app = express();
 
 app.get('/', (req, res) => {
